beed97dbf4ee01f675a35fd3ef5fdede098804a7
Yael Martinez <13.ymartinez.2b@gmail.com>
2021-08-29 21:29:45 -0500

    /demo 418 response

--- app.js
+++ app.js
@@ -6,4 +6,11 @@ app.get('/', (req, res) => {
 	res.send('Ya quiero una pta Startup');
 });
 
+app.get('/demo', (req, res) => {
+	res.set('X-full-stacl', '4life');
+	res.status(418);
+	res.send('I prefer coffe');
+});
+
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
